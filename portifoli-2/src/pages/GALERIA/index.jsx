577e93b708025e0ca39fb8909e96da54d567e494
Breno Santos <seu-email-do-github@gmail.com>
2026-06-04 16:38:30 -0300

    img lazaro

--- portifoli-2/src/pages/GALERIA/index.jsx
+++ portifoli-2/src/pages/GALERIA/index.jsx
@@ -21,7 +21,7 @@ import imagem6 from '../../assets/img/lazaro.jpeg'
 
 const imagens = [
   
-  { id: 6, url: imagem1, },
+  { id: 6, url: imagem6, },
   { id: 5, url: imagem1, },
   { id: 2, url: imagem2, },
   { id: 3, url: imagem3, },
